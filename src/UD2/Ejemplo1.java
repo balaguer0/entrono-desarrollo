09cb41675ae883ca68763ba32154f03476789ecb
Carlos Ariel <152276657+balaguer0@users.noreply.github.com>
2023-11-28 12:53:08 +0100

    Update Ejemplo1.java

--- src/UD2/Ejemplo1.java
+++ src/UD2/Ejemplo1.java
@@ -7,6 +7,8 @@ public class Ejemplo1 {
 	public static void main(String[] args) {
 		for (int i=0;i<1000;i++) {
 			System.out.println(i);
+			System.out.println(i);
+			System.out.println(i);
 		}
 
 	}
